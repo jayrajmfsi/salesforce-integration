3891411047af9a16fd9e593fc9c32696b6d6e4cf
JAYRAJ ARORA <jayraja@mindfiresolutions.com>
2019-04-27 04:26:56 +0530

    Removed the error reporting

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
  * Index file for showing the login button
  * @author Jayraj Arora<jayraja@mindfiresolutions.com>
  */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 use SalesForce\ApiCaller\ApiCaller;
 
